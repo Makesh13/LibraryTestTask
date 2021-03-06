2d96a22ce12c9f1ab7ce4a8e6d0bdd36948cd61a
Makesh13 <57521715+Makesh13@users.noreply.github.com>
2021-03-06 17:28:40 +0300

    Add angular project

---
+++ Common/Migrations/20210306140846_Change model book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Common.Migrations
+{
+    public partial class Changemodelbook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("e83b4bec-765b-4e13-a349-611ad5cda0cb"),
+                column: "Author",
+                value: "Терри Пратчет");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+        }
+    }
+}
